575fcc999692fa1dce3156420f8f4cc810db3aa7
jac <joshuacarpentier21@gmail.com>
2019-11-12 15:34:08 -0500

    updated code

--- 40..ValidCreditCardNumber/src/DriverStockLookUp.java
+++ 40..ValidCreditCardNumber/src/DriverStockLookUp.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class DriverStockLookUp {
 
@@ -17,9 +19,36 @@ public class DriverStockLookUp {
 		
 		float price = stockLookUp.get("IBM");
 		System.out.println(price);
+		
+		
+	Map<String, Stock> advStockLookUp = new HashMap<>();
+		
 	
+	BiConsumer<Integer, Integer> x = DriverStockLookUp::boobboo;
+	
+	Consumer<Float> y = DriverStockLookUp::boo;
+	// before parameters where <T t> this just represent datatype of object and its name and for the other part
+	// with the methods passing functions 
+	
+	advStockLookUp.put("IBM", new Stock ("IBM", 140f, .03f) );
+	advStockLookUp.put("SAN", new Stock ("SAN", 4f, .05f) );
+	advStockLookUp.put("TD", new Stock ("TD", 50f, .0325f) );
+	
+	advStockLookUp.forEach( (k,v) ->{
+		System.out.println(k + " " + v);
+	});
+	
+
 	
 	
 	}
+	
+	public static void boobboo(int a, int b) {
+		
+	}
+	
+	public static void boo(float x) {
 
+}
+	
 }
--- 40..ValidCreditCardNumber/src/Stock.java
+++ 40..ValidCreditCardNumber/src/Stock.java
@@ -1,8 +1,45 @@
 
 public class Stock {
-	
+
 	private String name;
 	private float price;
 	private float divYield;
 
+
+	public Stock(String name, float price, float divYield) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.divYield = divYield;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public float getDivYield() {
+		return divYield;
+	}
+
+	public void setDivYield(float divYield) {
+		this.divYield = divYield;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Stock [name=" + name + ", price=" + price + ", divYield=" + divYield + "]";
+	}
 }
